d2ba306321ef6b9df3331415a709b0c3d99af4b7
bmalvo <boydmalvo@gmail.com>
2025-04-15 14:37:31 +0200

    data types: int, float

--- basic.py
+++ basic.py
@@ -1,8 +1,37 @@
 # print
-print('patrick')
+# print('patrick')
 
 # input
 
-user = input('what is Your name?')
+# user = input('what is Your name?')
 
-print(f'Welcome {user}')
+# print(f'Welcome {user}')
+
+# Data types
+
+# int & float
+
+
+
+print(2 + 2)
+print(2 - 2)
+print(2 * 2)
+print(2 / 2)
+print(2 ** 2)
+print(13//4)
+print(13 % 4)
+print('rounded:', round(3.1))
+
+print(type(-3))
+print(type(1.2))
+print(type(None))
+
+# bool
+# str
+# list
+# tuple
+# set
+# dict
+# classes
+# specilized data types (modules)
+#  None 
