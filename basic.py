15891df153913e841b7eb6f19b6767ecdb60b075
bmalvo <boydmalvo@gmail.com>
2025-04-15 19:55:50 +0200

    variables

--- basic.py
+++ basic.py
@@ -33,8 +33,20 @@
 
 # binary
 
-print(bin(5))
-print(int(0b101, 2))
+# print(bin(5))
+# print(int(0b101, 2))
+
+# variables
+# snake_case start with lowercase or underscore
+#  letters, numbers, underscores, case sensetive
+# don't overwrite keywords
+
+iq = 190
+iq_user = 230
+_user = 10
+a, b, c = 1, 2, 3
+
+print(sum((iq, iq_user, _user)))
 
 # bool
 # str
