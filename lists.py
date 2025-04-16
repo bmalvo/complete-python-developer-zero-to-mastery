cc8568caa0cc9d1356745dccbb66f450a73a3fc1
bmalvo <boydmalvo@gmail.com>
2025-04-16 15:25:41 +0200

    list

---
+++ lists.py
@@ -0,0 +1,17 @@
+# list
+
+l1 = [1,2,3,4,5]
+l2 = ['a','b','c']
+l3 = [1,'a',False]
+
+amazon_cart = [
+    'notebook', 
+    'sunglass',
+    'toy',
+    'grape'
+    ]
+
+amazon_cart.append('gum')
+
+print(amazon_cart)
+
