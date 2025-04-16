103cbf15096f6e4afda37c6bde95e52e961dc250
bmalvo <boydmalvo@gmail.com>
2025-04-16 21:28:10 +0200

    list unpacking

--- lists.py
+++ lists.py
@@ -27,26 +27,35 @@
 
 # list methods
 
-basket = [1,2,3,4,5]
+# basket = [1,2,3,4,5]
 
 #  adding
 
-basket.append(6)
-basket.insert(3, 3.5)
-basket.extend([7,8])
-basket.pop()
-basket.remove(3.5)
+# basket.append(6)
+# basket.insert(3, 3.5)
+# basket.extend([7,8])
+# basket.pop()
+# basket.remove(3.5)
 # basket.clear()
-basket.index(2)
+# basket.index(2)
 
-print(basket)
-print(9 if 2 in basket else 'nope')
+# print(basket)
+# print(9 if 2 in basket else 'nope')
 
-not_sorted = ['b', 'e', 'y', 'x', 'm', 'a']
-not_sorted.sort(reverse=True)
-print(not_sorted)
+# not_sorted = ['b', 'e', 'y', 'x', 'm', 'a']
+# not_sorted.sort(reverse=True)
+# print(not_sorted)
 
-print(list(range(100)))
+# print(list(range(100)))
 
-strings_list = ['my', 'name', 'is', 'brydzia']
-print('-'.join(strings_list))
+# strings_list = ['my', 'name', 'is', 'brydzia']
+# print('-'.join(strings_list))
+
+# list unpacking
+
+a,b,c, *rest = 1,2,3,4,5,6,7,8,9,0
+
+print(a)
+print(b)
+print(c)
+print(rest)
