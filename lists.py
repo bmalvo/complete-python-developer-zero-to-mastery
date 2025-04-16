af8e133855c004b10db2fa68891ee16a5a8fc3cd
bmalvo <boydmalvo@gmail.com>
2025-04-16 18:07:59 +0200

    list methods

--- lists.py
+++ lists.py
@@ -1,17 +1,47 @@
 # list
 
-l1 = [1,2,3,4,5]
-l2 = ['a','b','c']
-l3 = [1,'a',False]
+# l1 = [1,2,3,4,5]
+# l2 = ['a','b','c']
+# l3 = [1,'a',False]
 
-amazon_cart = [
-    'notebook', 
-    'sunglass',
-    'toy',
-    'grape'
-    ]
+# amazon_cart = [
+#     'notebook', 
+#     'sunglass',
+#     'toy',
+#     'grape'
+#     ]
 
-amazon_cart.append('gum')
+# amazon_cart.append('gum')
 
-print(amazon_cart)
+# print(amazon_cart)
 
+# matrix
+
+# matrix = [
+#     [1,0,1],
+#     [0,1,0],
+#     [1,0,1]
+# ]
+
+# print(matrix[0][1])
+
+# list methods
+
+basket = [1,2,3,4,5]
+
+#  adding
+
+basket.append(6)
+basket.insert(3, 3.5)
+basket.extend([7,8])
+basket.pop()
+basket.remove(3.5)
+# basket.clear()
+basket.index(2)
+
+print(basket)
+print(9 if 2 in basket else 'nope')
+
+not_sorted = ['b', 'e', 'y', 'x', 'm', 'a']
+not_sorted.sort(reverse=True)
+print(not_sorted)
