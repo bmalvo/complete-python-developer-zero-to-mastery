fde887014e84f5ec52c25a93def7c276adcdb5a3
bmalvo <boydmalvo@gmail.com>
2025-04-17 08:28:38 +0200

    dictionaries

---
+++ dictionaries.py
@@ -0,0 +1,13 @@
+# dictionary
+
+dict = {
+    'a': 1,
+    'b': 2,
+    'weapons': ['AK47', 'Beryl', 'Grot']
+}
+
+print(dict.get('c', 3))
+print(dict.keys())
+print(dict)
+
+print(dict['weapons'])
