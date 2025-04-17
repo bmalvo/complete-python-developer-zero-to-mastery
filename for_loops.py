5dc92c089a39520d47982c201e26cba479c23f46
bmalvo <boydmalvo@gmail.com>
2025-04-17 21:33:37 +0200

    for loops

---
+++ for_loops.py
@@ -0,0 +1,19 @@
+#/ for loops
+ 
+for num in (1,2,3,4,5):
+    print(num)
+
+print(num)
+
+user = {
+    'name': 'Golem',
+    'age': 5006,
+    'can_swim': False
+}
+
+for k, v in user.items():
+    print(k, v)
+
+for numb in range(101):
+    if numb % 3 == 0:
+        print(numb)
