f357fcba49585b015b433b04b5adb2b0c24357a6
bmalvo <boydmalvo@gmail.com>
2025-04-18 08:14:17 +0200

    while loop

--- for_loops.py
+++ for_loops.py
@@ -1,19 +1,27 @@
 #/ for loops
  
-for num in (1,2,3,4,5):
-    print(num)
+# for num in (1,2,3,4,5):
+#     print(num)
 
-print(num)
+# print(num)
 
-user = {
-    'name': 'Golem',
-    'age': 5006,
-    'can_swim': False
-}
+# user = {
+#     'name': 'Golem',
+#     'age': 5006,
+#     'can_swim': False
+# }
 
-for k, v in user.items():
-    print(k, v)
+# for k, v in user.items():
+#     print(k, v)
 
-for numb in range(101):
-    if numb % 3 == 0:
-        print(numb)
+# for numb in range(101):
+#     if numb % 3 == 0:
+#         print(numb)
+
+# while loop
+
+i = 0
+
+while i < 10:
+    print(i)
+    i += 1
