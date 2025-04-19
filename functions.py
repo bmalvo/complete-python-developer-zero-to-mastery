5022fe478843f868f68ae555fd13d7951dacd334
bmalvo <boydmalvo@gmail.com>
2025-04-19 10:50:22 +0200

    functions parameters

--- functions.py
+++ functions.py
@@ -1,6 +1,12 @@
 # functions (DRY)
+# parameters
 
-def say_hello():
-    print('heellooo!')
 
-say_hello()
+def say_hello(name='unknown', emoji='🍕'):
+    return f'Heellooo {name}!{emoji}'
+
+
+print(say_hello('Stefka', '🚀'))
+print(say_hello('Brydzia'))
+
+print("".join(['a','b','c']))
