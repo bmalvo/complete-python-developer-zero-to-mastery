693e92cd08818b4f08dbee2df562294dcd70ded8
bmalvo <boydmalvo@gmail.com>
2025-04-19 21:33:31 +0200

    highest even

--- functions.py
+++ functions.py
@@ -2,11 +2,20 @@
 # parameters
 
 
-def say_hello(name='unknown', emoji='🍕'):
-    return f'Heellooo {name}!{emoji}'
+# def say_hello(name='unknown', emoji='🍕'):
+#     return f'Heellooo {name}!{emoji}'
 
 
-print(say_hello('Stefka', '🚀'))
-print(say_hello('Brydzia'))
+# print(say_hello('Stefka', '🚀'))
+# print(say_hello('Brydzia'))
 
-print("".join(['a','b','c']))
+# print("".join(['a','b','c']))
+
+evens = [6, 10, 2, 3, 4, 8, 11]
+
+def highest_even(data):
+    '''output highest even number from data'''
+    highest = [num for num in data if num % 2 == 0]
+    return max(highest)
+
+print(highest_even(evens))
