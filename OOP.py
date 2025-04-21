e29712d471d0699e37d3e73484198949595d1bce
bmalvo <boydmalvo@gmail.com>
2025-04-21 11:31:01 +0200

    classmethod

--- OOP.py
+++ OOP.py
@@ -11,6 +11,10 @@ class player_character:
     def shout(self):
         print(f'My name is {self.name}')
 
+    @classmethod
+    def adding_things(cls, num1, num2):
+        return cls('Teddy', num1 + num2)
+
 
 player1 = player_character('Stefka', 15)
 player2 = player_character('Brydzia', 14)
@@ -21,3 +25,7 @@ print(player2.name, player2.age)
 print(player3.name, player3.age)
 
 player1.shout()
+
+player4 = player_character.adding_things(2,3)
+
+print(player4.name, player4.age)
