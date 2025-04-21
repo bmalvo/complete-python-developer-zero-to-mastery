e731fdc845096fab88ade811507380b35032003f
bmalvo <boydmalvo@gmail.com>
2025-04-21 11:40:13 +0200

    encapsulation

--- OOP.py
+++ OOP.py
@@ -1,4 +1,5 @@
 # OOP
+# Encapsulation is bundling data and the methods that operate on that data within a single unit, often a class, and restricting direct access to some of the object's components.
 
 class player_character:
     def __init__(self, name='unknown', age=0):
@@ -8,8 +9,8 @@ class player_character:
     def run(self):
         print('run')
 
-    def shout(self):
-        print(f'My name is {self.name}')
+    def speak(self):
+        print(f'My name is {self.name}, and I am {self.age} years old')
 
     @classmethod
     def adding_things(cls, num1, num2):
@@ -24,7 +25,7 @@ print(player1.name, player1.age)
 print(player2.name, player2.age)
 print(player3.name, player3.age)
 
-player1.shout()
+player1.speak()
 
 player4 = player_character.adding_things(2,3)
 
