6d1fc08207fccd1bf95387565e10e6f19064c4f3
bmalvo <boydmalvo@gmail.com>
2025-04-21 11:17:34 +0200

    defoult attributes

--- OOP.py
+++ OOP.py
@@ -1,7 +1,7 @@
 # OOP
 
 class player_character:
-    def __init__(self, name, age):
+    def __init__(self, name='unknown', age=0):
         self.name = name
         self.age = age
 
@@ -14,8 +14,10 @@ class player_character:
 
 player1 = player_character('Stefka', 15)
 player2 = player_character('Brydzia', 14)
+player3 = player_character()
 
-# print(player1.name, player1.age)
-# print(player2.name, player2.age)
+print(player1.name, player1.age)
+print(player2.name, player2.age)
+print(player3.name, player3.age)
 
 player1.shout()
