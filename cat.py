73c9f707d4d624981676acf8e5383a492c3793f0
bmalvo <boydmalvo@gmail.com>
2025-04-21 11:25:34 +0200

    cat exercise resolve

--- cat.py
+++ cat.py
@@ -8,9 +8,20 @@ class Cat:
 
 
 # 1 Instantiate the Cat object with 3 cats
+cat1 = Cat('Stefka', 15)
+cat2 = Cat('Brydzia', 14)
+cat3 = Cat('Ninja', 5)
 
 
 # 2 Create a function that finds the oldest cat
-
+def find_oldest_cat(cats):
+    oldest_cat = cats[0]
+    for cat in cats:
+        if cat.age > oldest_cat.age:
+            oldest_cat = cat
+    return oldest_cat
 
 # 3 Print out: "The oldest cat is x years old.". x will be the oldest cat age by using the function in #2
+
+oldest_cat = find_oldest_cat([cat1, cat2, cat3])
+print(f'The oldest cat is {oldest_cat.age} years old.')
