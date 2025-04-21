195582709db98715d4dca12cdb7894a22c832522
bmalvo <boydmalvo@gmail.com>
2025-04-21 11:00:12 +0200

    add class player_character

--- OOP.py
+++ OOP.py
@@ -1,13 +1,16 @@
 # OOP
 
-class Animal:
-    def __init__(self, name, species):
+class player_character:
+    def __init__(self, name, age):
         self.name = name
-        self.species = species
+        self.age = age
 
-    def make_sound(self, sound):
-        return f"{self.name} says {sound}"
+    def run(self):
+        print('run')
 
-# Example usage
-dog = Animal("Buddy", "Dog")
-print(dog.make_sound("Woof"))
+
+player1 = player_character('Stefka', 15)
+player2 = player_character('Brydzia', 14)
+
+print(player1.name, player1.age)
+print(player2.name, player2.age)
