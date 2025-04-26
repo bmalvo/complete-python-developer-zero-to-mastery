5af0dfa86b5d439792dec9bd2ea59dc27eaf30ef
bmalvo <boydmalvo@gmail.com>
2025-04-26 21:29:46 +0200

    modules, packages

---
+++ modules/main.py
@@ -0,0 +1,6 @@
+from utility import multiply
+from shoping.shoping_cart import buy
+
+print(multiply(2,3))
+
+print(buy('apple'))
---
+++ modules/shoping/shoping_cart.py
@@ -0,0 +1,4 @@
+def buy(item):
+    item_list = []
+    item_list.append(item)
+    return item_list
