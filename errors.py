7fe01eaae05a89b4b8b9da829595c4eba471116a
bmalvo <boydmalvo@gmail.com>
2025-04-26 14:10:22 +0200

    errors vol 2

--- errors.py
+++ errors.py
@@ -1,16 +1,38 @@
 # Errors
 
-try:
-    print([3 + 'wow!'])
-except:
-    print('something goes wrong')
+# try:
+#     print([3 + 'wow!'])
+# except:
+#     print('something goes wrong')
+
+# while True:
+#     try:
+#         age = int(input('What is Your name? '))
+#         print(age)
+#     except:
+#         print('please enter a number')
+#     else:
+#         print('thank You!')
+#         break
+
+# def sum(num1, num2):
+#     try:
+#         return num1 + num2
+#     except (TypeError, ZeroDivisionError) as err:
+#         return f'please enter a number {err}'
+
+# print(sum('1', 2))
 
 while True:
     try:
-        age = int(input('What is Your name? '))
-        print(age)
-    except:
+        age = int(input('What is Your age?'))
+        10/age
+    except ValueError:
         print('please enter a number')
-    else:
+    except ZeroDivisionError:
+        print('please enter a number higher than 0')
+    else: 
         print('thank You!')
         break
+    finally:
+        print('ok, i am finally done.')
