8b49f90f9e341a7be0fc67d4c74caf0ec4e79b05
bmalvo <boydmalvo@gmail.com>
2025-04-27 11:51:48 +0200

    random

--- modules/main.py
+++ modules/main.py
@@ -1,6 +1,17 @@
 from utility import multiply
 from shoping.shoping_cart import buy
+from random import random,randint,shuffle, choice
 
 print(multiply(2,3))
 
 print(buy('apple'))
+
+print(random())
+print(randint(0, 10))
+print(choice([1,2,3,4,5,6,7,8,9,0]))
+
+my_list = ['a','b','c','d','e','f']
+
+shuffle(my_list)
+
+print(my_list)
