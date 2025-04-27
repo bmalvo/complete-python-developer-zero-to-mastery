272892e36593ae155aa1a2c84ee0b67b45db004f
bmalvo <boydmalvo@gmail.com>
2025-04-27 13:09:44 +0200

    usefull collctions

---
+++ modules/usefull_modules/collections_usefull.py
@@ -0,0 +1,10 @@
+from collections import Counter, defaultdict
+
+sentence = 'Stefka is a very dangerous cat'
+
+print(Counter(sentence))
+
+dictionary = defaultdict(int,{'a': 1, 'b': 2})
+
+print(dictionary['c'])
+
